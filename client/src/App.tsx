c32faf0da79ccfe31a1cda2447d67536f337d9f9
Habib Balde <hab98@drexel.edu>
2025-07-11 03:10:34 -0400

    final commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,19 +3,17 @@ import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
-
-
-export const BASE_URL = import.meta.env.MODE === "development" ?  "http://localhost:9000/api": "api/";
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:9000/api" : "/api";
 
 function App() {
 	return (
-    <Stack h="100vh">
-      <Navbar />
-      <Container>
-        <TodoForm/>
-        <TodoList/>
-      </Container>
-    </Stack>
+		<Stack h='100vh'>
+			<Navbar />
+			<Container>
+				<TodoForm />
+				<TodoList />
+			</Container>
+		</Stack>
 	);
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,20 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import theme from "./Chakra/theme.ts";
 
-
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+		<QueryClientProvider client={queryClient}>
 			<ChakraProvider theme={theme}>
 				<App />
 			</ChakraProvider>
-    </QueryClientProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
-
 
